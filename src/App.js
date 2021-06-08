0d428b865dde79106089c412d1ba31677953e615
지혜인(Hyein Ji)/Media Integration팀/SKP <hyein.ji@sk.com>
2021-06-08 17:49:07 +0900

    8.6 Reactive Variables

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
+import { useReactiveVar } from "@apollo/client";
+import { BrowserRouter as Router, Route, Switch/*, Redirect*/ } from "react-router-dom";
+import Home from "./screens/Home";
+import Login from "./screens/Login";
+import NotFound from "./screens/NotFound";
+import { isLoggedInVar } from "./apollo";
+
 function App() {
+  const isLoggedIn = useReactiveVar(isLoggedInVar);
   return (
-    <div></div>
+    <div>
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            { isLoggedIn? (
+              <Home />
+            ): (
+              <Login /> 
+            )}
+          </Route>
+          <Route>
+            <NotFound />
+            {/* <Redirect to="/" /> */}
+          </Route>
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
