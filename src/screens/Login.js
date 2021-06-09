65b15a6c0c8eaf255219be4f6942a42c360e13e6
지혜인(Hyein Ji)/Media Integration팀/SKP <hyein.ji@sk.com>
2021-06-09 16:38:38 +0900

    10.11 Login part Two

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,3 +1,4 @@
+import { gql, useMutation } from "@apollo/client";
 import {
     faFacebookSquare,
     faInstagram,
@@ -14,6 +15,7 @@ import BottomBox from "../components/auth/BottomBox";
 import PageTitle from "../components/PageTitle";
 import routes from "../routes";
 import FormError from "../components/auth/FormError";
+import { logUserIn } from "../apollo";
   
 const FacebookLogin = styled.div`
     color: #385285;
@@ -22,15 +24,47 @@ const FacebookLogin = styled.div`
       font-weight: 600;
     }
 `;
+
+const LOGIN_MUTATION = gql`
+    mutation login($username:String!, $password:String!) {
+        login(username:$username, password:$password) {
+            ok
+            token
+            error
+        }
+    }
+`;
   
 function Login() {
-    const { register, handleSubmit, errors, formState } = useForm({
+    const { register, handleSubmit, errors, formState, getValues, setError, clearErrors } = useForm({
         mode: "onChange", // onBlur
     });
+    const onCompleted = (data) => {
+        const { login: { ok, error, token }} = data;
+        if (!ok) {
+            return setError("result", {
+                message: error,
+            });
+        }
+        if (token) {
+            logUserIn(token);
+        }
+    }
+    const [login, { loading }] = useMutation(LOGIN_MUTATION, {
+        onCompleted,
+    });
     const onSubmitValid = (data) => {
-        // console.log(data);
+        if (loading) {
+            return;
+        }
+        const { username, password } = getValues(); 
+        login({
+            variables: { username, password }
+        });
     };
-    // console.log(errors, formState.isValid);
+    const clearLoginError = () => {
+        clearErrors("result");
+    }
     return (
         <AuthLayout>
             <PageTitle title="Login" />
@@ -50,6 +84,7 @@ function Login() {
                         // pattern: "", // 정규식
                         // validate: (currentValue) => currentValue.includes("potato"),
                     })} 
+                    onChange={clearLoginError}
                     name="username" 
                     type="text" 
                     placeholder="Username"
@@ -60,13 +95,19 @@ function Login() {
                     ref={register({
                         required: "Password is required.",
                     })}
+                    onChange={clearLoginError}
                     name="password"
                     type="password"
                     placeholder="Password"
                     hasError={Boolean(errors?.password?.message)}
                 />
                 <FormError message={errors?.password?.message} />
-                <Button type="submit" value="Log in" disabled={!formState.isValid} />
+                <Button
+                    type="submit"
+                    value={ loading? "Loading..." : "Log in" }
+                    disabled={!formState.isValid || loading}
+                />
+                <FormError message={errors?.result?.message} />
             </form>
             <Separator />
             <FacebookLogin>
